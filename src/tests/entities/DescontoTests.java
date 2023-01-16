b3bdc0a970644964d2f02161112ef876547692d8
LeoDominici71 <leonardo_requena@outlook.com>
2023-01-16 14:10:15 +0000

    implementando Factory

--- src/tests/entities/DescontoTests.java
+++ src/tests/entities/DescontoTests.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Assertions;
 import entities.DetalhePromocao;
 import entities.Livro;
 import entities.enums.TipoPromocaoEnum;
+import tests.factory.DescontoFactory;
 
 public class DescontoTests {
 	
@@ -18,8 +19,8 @@ public class DescontoTests {
 		
 		double price = 31;
 		double expectedValue = 29.45;
-		Livro livroTest = new Livro("X", "X", price);
-		DetalhePromocao detP = new DetalhePromocao(new Date("2023/01/10"), new Date("2023/01/15"), livroTest,TipoPromocaoEnum.valueOf("A"), 5);
+		Livro livroTest = DescontoFactory.createEmptyLivro(price);
+		DetalhePromocao detP = new DetalhePromocao(new Date("2023/01/10"), new Date("2023/01/15"), livroTest,TipoPromocaoEnum.valueOf("A"), DescontoFactory.PctDescontoA);
 		List<DetalhePromocao> list = new ArrayList<>();
 		detP.desconto(detP.getTipo(),livroTest , detP.getPctPromocao());
 		Assertions.assertEquals(expectedValue, detP.desconto(detP.getTipo(), livroTest, detP.getPctPromocao()));
@@ -31,8 +32,8 @@ public class DescontoTests {
 		
 		double price = 31;
 		double expectedValue = 27.9;
-		Livro livroTest = new Livro("X", "X", price);
-		DetalhePromocao detP = new DetalhePromocao(new Date("2023/01/10"), new Date("2023/01/20"), livroTest,TipoPromocaoEnum.valueOf("B"), 10);
+		Livro livroTest = DescontoFactory.createEmptyLivro(price);
+		DetalhePromocao detP = new DetalhePromocao(new Date("2023/01/10"), new Date("2023/01/20"), livroTest,TipoPromocaoEnum.valueOf("B"), DescontoFactory.PctDescontoB);
 		List<DetalhePromocao> list = new ArrayList<>();
 		detP.desconto(detP.getTipo(),livroTest , detP.getPctPromocao());
 		Assertions.assertEquals(expectedValue, detP.desconto(detP.getTipo(), livroTest, detP.getPctPromocao()));
@@ -44,8 +45,8 @@ public class DescontoTests {
 		
 		double price = 31;
 		double expectedValue = 26.35;
-		Livro livroTest = new Livro("X", "X", price);
-		DetalhePromocao detP = new DetalhePromocao(new Date("2023/01/10"), new Date("2023/01/30"), livroTest,TipoPromocaoEnum.valueOf("C"), 15);
+		Livro livroTest = DescontoFactory.createEmptyLivro(price);
+		DetalhePromocao detP = new DetalhePromocao(new Date("2023/01/10"), new Date("2023/01/30"), livroTest,TipoPromocaoEnum.valueOf("C"), DescontoFactory.PctDescontoC);
 		List<DetalhePromocao> list = new ArrayList<>();
 		detP.desconto(detP.getTipo(),livroTest , detP.getPctPromocao());
 		Assertions.assertEquals(expectedValue, detP.desconto(detP.getTipo(), livroTest, detP.getPctPromocao()));
---
+++ src/tests/factory/DescontoFactory.java
@@ -0,0 +1,24 @@
+package tests.factory;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import entities.DetalhePromocao;
+import entities.Livro;
+import entities.enums.TipoPromocaoEnum;
+
+public class DescontoFactory {
+	public static final int PctDescontoA = 5;
+	public static final int PctDescontoB = 10;
+	public static final int PctDescontoC = 15;
+	
+	public static Livro createEmptyLivro(double price) {
+		return new Livro("X", "X", price);
+	}
+	
+	
+}
